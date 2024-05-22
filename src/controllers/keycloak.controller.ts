51f78ec2597021688f1aec3f75052db9b3be7c3e
Sagar das <sagarmikeylevi@gmail.com>
2024-05-22 13:27:56 +0530

    keyclock: refresh token

--- src/controllers/keycloak.controller.ts
+++ src/controllers/keycloak.controller.ts
@@ -51,6 +51,32 @@ export const session = async (req: Request, res: Response) => {
 
     res.status(200).json(response.data);
   } catch (error) {
-    console.log(error);
+    console.error("Failed to login:", error);
+    res.status(500).json({ error: "Failed to login" });
+  }
+};
+
+export const refreshToken = async (req: Request, res: Response) => {
+  const { refreshToken } = req.body;
+  try {
+    const response = await axios.post(
+      `${keycloakConfig.baseUrl}/realms/${keycloakConfig.realmName}/protocol/openid-connect/token`,
+      {
+        grant_type: "refresh_token",
+        client_id: keycloakConfig.clientId,
+        client_secret: keycloakConfig.clientSecret,
+        refresh_token: refreshToken,
+      },
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error("Failed to refresh token:", error);
+    res.status(500).json({ error: "Failed to refresh token" });
   }
 };
--- src/routes/keycloak.ts
+++ src/routes/keycloak.ts
@@ -1,12 +1,17 @@
 import { Request, Response, Router } from "express";
 import { authenticateKeycloakAdmin } from "../middleware/authenticateKeycloakAdmin.js";
-import { createUser, session } from "../controllers/keycloak.controller.js";
+import {
+  createUser,
+  refreshToken,
+  session,
+} from "../controllers/keycloak.controller.js";
 import { authKeycloakToken } from "../middleware/authKeycloakToken.js";
 
 const keycloakRouter = Router();
 
 keycloakRouter.post("/register", authenticateKeycloakAdmin, createUser);
 keycloakRouter.post("/login", session);
+keycloakRouter.post("/refresh-token", refreshToken);
 keycloakRouter.get(
   "/protected",
   authKeycloakToken,
